d06d05139cd1daa819421db31cf9b30656b04e2a
2roy999 <2903801+2roy999@users.noreply.github.com>
2020-01-20 22:44:14 +0200

    Change mongoose settings to fix warnings (#4)

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports.connect = (uri) => {
-	mongoose.connect(uri, {useMongoClient: true});
+	mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 	// plug in the promise library:
 	mongoose.Promise = global.Promise;
 
